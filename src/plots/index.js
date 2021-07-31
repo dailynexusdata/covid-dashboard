c9f53d14e64229ae830fdb36c658def42f44c36f
alrudolph <alex3r@me.com>
2021-07-30 21:39:14 -0700

    updates

--- src/plots/index.js
+++ src/plots/index.js
@@ -7,14 +7,17 @@ import makeVaccineTypes from "./vaccineTypes";
   const convertTime = d3.timeParse("%Y-%m-%d");
 
   const vaccineData = await d3.csv(
-    "https://raw.githubusercontent.com/dailynexusdata/covid-dashboard/main/dist/data/vaccines.csv"
+    "https://raw.githubusercontent.com/dailynexusdata/covid-dashboard/main/dist/data/vaccines.csv",
+    (d) => {
+      return {
+        date: convertTime(d.date),
+        cumulative_pfizer_doses: +d.cumulative_pfizer_doses,
+        cumulative_moderna_doses: +d.cumulative_moderna_doses,
+        cumulative_jj_doses: +d.cumulative_jj_doses,
+      };
+    }
   );
-  vaccineData.forEach((d) => {
-    d["date"] = convertTime(d["date"]);
-    d.cumulative_pfizer_doses = +d.cumulative_pfizer_doses;
-    d.cumulative_moderna_doses = +d.cumulative_moderna_doses;
-    d.cumulative_jj_doses = +d.cumulative_jj_doses;
-  });
+  console.log(vaccineData.columns);
 
   window.addEventListener("resive", () => {
     resize();
