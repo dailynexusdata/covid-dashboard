e164c2a1a3560cd21fcbf3f38826a0a0fdbe807e
alrudolph <alex3r@me.com>
2021-07-30 20:45:19 -0700

    updates

--- src/plots/index.js
+++ src/plots/index.js
@@ -1,4 +1,5 @@
 import * as d3 from "d3";
+import "./styles.scss";
 
 import makeVaccineTypes from "./vaccineTypes";
 
--- webpack.config.js
+++ webpack.config.js
@@ -3,10 +3,25 @@ const path = require("path");
 const config = {
   entry: "./src/plots/index.js",
   output: {
-    filename: "bundle.js",
+    filename: "covid-dashboard-bundle.js",
     path: path.resolve(__dirname, "dist"),
   },
   plugins: [],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          "style-loader",
+          // Translates CSS into CommonJS
+          "css-loader",
+          // Compiles Sass to CSS
+          "sass-loader",
+        ],
+      },
+    ],
+  },
 };
 
 module.exports = () => config;
