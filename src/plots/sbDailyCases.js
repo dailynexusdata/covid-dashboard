77ac6ec027aff44aaf714d16912c4310408b8ffd
zachgoldy <bloonsmstr54@gmail.com>
2021-08-20 14:53:16 -0700

    added rectangles and line for dailyCases graph

--- src/plots/sbDailyCases.js
+++ src/plots/sbDailyCases.js
@@ -7,12 +7,13 @@
 import * as d3 from 'd3';
 
 /**
-  * @param {*} data - dailyCases.csv
-  *
-  * @author Zach
-  *
-  * @since 08/19/2021
-  */
+ * @param {*} data - dailyCases.csv
+ *
+ * @author Zach
+ *
+ * @since 08/19/2021
+ */
+
 const makeDailyCases = (data) => {
   /*
      Container Setup:
@@ -43,10 +44,7 @@ const makeDailyCases = (data) => {
     .attr('height', size.height)
     .attr('width', size.width);
 
-  container
-    .append('a')
-    .text('Source: __________')
-    .attr('href', '');
+  container.append('a').text('Source: __________').attr('href', '');
 
   /*
      Create Scales:
@@ -65,7 +63,7 @@ const makeDailyCases = (data) => {
   // Axes
   svg
     .append('g')
-    .style('font-family', 'Helvetica Neue,Helvetica,Arial,sans-serif')
+    .style('color', '#adadad')
     .style('font-size', '12pt')
     .attr('transform', `translate(0, ${size.height - margin.bottom})`)
     .call(
@@ -73,18 +71,61 @@ const makeDailyCases = (data) => {
         .axisBottom()
         .scale(x)
         .tickFormat((d) => {
-          const t = d3.timeFormat('%b \'%g')(d);
+          const t = d3.timeFormat("%b '%g")(d);
           return t;
         }),
-
     );
+  const horizLines = svg.append('g');
+  y.ticks(4).slice(1).forEach((yVal) => {
+    horizLines
+      .append('line')
+      .attr('x1', margin.left)
+      .attr('x2', size.width - margin.right)
+      .attr('y1', y(yVal))
+      .attr('y2', y(yVal))
+      .attr('stroke', '#d3d3d3')
+      .attr('stroke-width', '0.5px');
 
-  console.log(y.ticks(3).slice(1));
+    horizLines
+      .append('text')
+      .text(yVal)
+      .style('font-size', '10pt')
+      .attr('fill', '#adadad')
+      .attr('x', margin.left)
+      .attr('y', y(yVal) - 5);
+  });
   /*
      Start Plot:
    */
-  data.forEach((yVal) => {
 
-  });
+  const bars = svg
+    .append('g')
+    .attr('fill', '#85BDDEbb')
+    .selectAll('bars')
+    .data(data)
+    .enter()
+    .append('rect')
+    .attr('x', (d) => x(d.date))
+    .attr('width', 1)
+    .attr('y', (d) => y(d.cases))
+    .attr('height', (d) => size.height - margin.bottom - y(d.cases));
+    /* if (y(d.cases) < 0) {
+        console.log(d);
+        return size, height;
+      } */
+  const line = d3
+    .line()
+    .x((d) => x(d.date))
+    .y((d) => y(d.avg));
+
+  svg
+    .append('g')
+    .datum(data)
+    .append('path')
+    .attr('d', line)
+    .attr('stroke', '#D96942')
+    .attr('stroke-width', 2)
+    .attr('fill', 'none');
 };
+
 export default makeDailyCases;
